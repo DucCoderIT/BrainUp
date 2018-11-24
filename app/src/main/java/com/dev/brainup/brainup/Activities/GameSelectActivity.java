b979531704f06b2811b9f1070a7205408e746569
TrungDuc <it.code.duc@gmail.com>
2018-11-24 09:05:26 +0700

    24-11/9h05
    Signed-off-by: TrungDuc <it.code.duc@gmail.com>

--- app/src/main/java/com/dev/brainup/brainup/Activities/GameSelectActivity.java
+++ app/src/main/java/com/dev/brainup/brainup/Activities/GameSelectActivity.java
@@ -19,6 +19,9 @@ import com.dev.brainup.brainup.DBHelper;
 import com.dev.brainup.brainup.R;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class GameSelectActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView imgView_game;
@@ -51,19 +54,26 @@ public class GameSelectActivity extends AppCompatActivity implements View.OnClic
                 String result = game_type_list.get(countGame).getResulttrue();
                 if (value.equalsIgnoreCase(result)){
                     Toast.makeText(this, "Chúc mừng, bạn đã trả lời chính xác", Toast.LENGTH_SHORT).show();
-                    countGame +=1;
-                    setInfoGame(countGame);
+                    if (countGame < game_type_list.size() -1) {
+                        countGame +=1;
+                        setInfoGame(countGame);
+                    }else Toast.makeText(this, "Bạn quá giỏi! Hết game của tui rồi!", Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(this, "Sai rồi bạn ơi! Chơi lại từ đầu nhé!", Toast.LENGTH_SHORT).show();
+                    countGame =0;
+                    setInfoGame(countGame);
                 }
                 break;
         }
     }
+
+    //override button back
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         Toast.makeText(this, "pause", Toast.LENGTH_SHORT).show();
         return super.onKeyDown(keyCode, event);
     }
+
     private void handle(){
         imgView_game = (ImageView) findViewById(R.id.imgview_game);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
@@ -72,20 +82,45 @@ public class GameSelectActivity extends AppCompatActivity implements View.OnClic
         rdBtn_result3 = (RadioButton) findViewById(R.id.rdbtn_result3);
         btn_sendResult = (Button) findViewById(R.id.btn_sendresult);
         btn_sendResult.setOnClickListener(this);
+        //get value from gametype activity
         typeGame = getIntent().getExtras().getString("type");
         Log.d(TAG, "type: "+ typeGame);
     }
+
+    //get all game with type
     private void getInfoGame(){
         DBHelper dbHelper = new DBHelper(this);
         Game game = new Game();
         game_type_list = dbHelper.getAllGameType(typeGame);
-        Log.d(TAG, "getInfoGame: "+game_type_list.size());
+        Log.d(TAG, "getInfoGame: size list: "+game_type_list.size());
     }
+    //set data for game
     private void setInfoGame(int position){
+        //set id image source ( @mipmap / getimage() )
         int id = getResources().getIdentifier(game_type_list.get(position).getImage(),"mipmap",getPackageName());
         imgView_game.setImageResource(id);
-        rdBtn_result1.setText(game_type_list.get(position).getResulttrue());
-        rdBtn_result2.setText(game_type_list.get(position).getResultfalse1());
-        rdBtn_result3.setText(game_type_list.get(position).getResultfalse2());
+        // get value result of game
+        String value1 = game_type_list.get(position).getResulttrue();
+        String value2 = game_type_list.get(position).getResultfalse1();
+        String value3 = game_type_list.get(position).getResultfalse2();
+        //add to list
+        List listValue = new ArrayList();
+        listValue.add(value1);
+        listValue.add(value2);
+        listValue.add(value3);
+        //set random list number
+        ArrayList<Integer> number = new ArrayList<Integer>();
+        for (int i = 0 ; i <= 2 ; ++i) number.add(i);
+        Collections.shuffle(number);
+        Log.d(TAG, "setInfoGame: random list:"+ number);
+        //use void setRdbtn to set result for radiobutton
+        setRdbtn(rdBtn_result1,listValue.get(number.get(0)).toString());
+        setRdbtn(rdBtn_result2,listValue.get(number.get(1)).toString());
+        setRdbtn(rdBtn_result3,listValue.get(number.get(2)).toString());
+    }
+
+    //set text for radio button
+    private void setRdbtn(RadioButton rdbtn,String Result){
+        rdbtn.setText(Result);
     }
 }
